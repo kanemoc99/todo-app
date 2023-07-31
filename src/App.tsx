7f322e64a007a928ed9ae6c8f7360b5b861bba1b
kanemoc99 <chinatsu.kanemoto@japan-systems.co.jp>
2023-07-31 22:50:07 +0900

    kadai2

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,39 @@ import { useState } from "react";
 import type { ChangeEvent } from 'react';
 
 
-  const App = () => {
+const App = () => {
+
+  type Todo = {
+    value: String,
+    text: String,
+    key: number;
+  };
   const [value, setValue] = useState("");
-  const [text, setText] = useState("");
+  const [text, setText] = useState<String[]>([]);
+
+  const addTodo = () => {
+    text.push(value)
+    setValue("");
+  }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.currentTarget.value)
   }
 
-  const handleClick = () => {
-    setText(value); 
-    setValue(""); 
-  }
-
-  console.log('コンポーネントの再描画が発生しているかどうかのチェック')
-
-
-    return (
-      <div className="App">
-        <form>        
-        <input type="text"  value={value} onChange={handleChange}/>
-        <button type="button" onClick={handleClick}>追加</button>        
-        <p>{text}</p>
-        </form>
-      </div>
-    );
-  };
+  return (
+    <div className="App">
+      <form>
+        <input type="text" value={value} onChange={handleChange} />
+        <button type="button" onClick={addTodo}>追加</button>
+        <ul>
+          {text.map((value, i) => {
+            return (
+              <li key={i}>{value}</li>)
+          })}
+        </ul>
+      </form>
+    </div>
+  );
+};
 
 export default App;
